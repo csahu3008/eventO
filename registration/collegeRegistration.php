aea41b3f0135ecfed79f9a0f912738e34bfc4f79
arunKumar <you@example.com>
2020-01-21 04:11:19 +0530

    collreg changes

--- registration/collegeRegistration.php
+++ registration/collegeRegistration.php
@@ -1,32 +1,4 @@
-<body>
-    <form action="" method="POST">
-        <div class="container">
-            <h1>Register College</h1>
-            <hr>
-            <label for="name"><b>College Name</b></label>
-            <input type="text" name="college_name" placeholder="Enter College Name" required>
 
-            <label for="name"><b>College Address</b></label>
-            <input type="text" name="college_address" placeholder="Enter College Address" required>
-
-            <label for="university_name"><b>University Name</b></label>
-            <input type="text" name="university_name" placeholder="Enter University Name" required>
-
-            <label for="phone"><b>Phone Number</b></label>
-            <input type="text"  placeholder="Enter Contact" name="phone" required>
-
-            <label for="psw"><b>Create New Password</b></label>
-            <input type="password" name="password" placeholder="Enter Password"  required>
-
-            <label for="psw-repeat"><b>Re-Enter Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="password-repeat" required>
-             <button type="submit" class="registerbtn">Register</button>
-        </div>
-
-        <div class="container signin">
-            <p>Already have an account? <a href="#">Sign in</a>.</p>
-        </div>
-    </form>
  <?php
 
 if(isset($_REQUEST['password']))
