9f2af9ad48fec958b0e32cc1f65947e5abd20c68
Chandra Shekhar <csahu3932@gmail.com>
2020-01-21 09:06:17 +0530

    studdash modified

--- dash/results.php
+++ dash/results.php
@@ -1,22 +1,15 @@
 <?php
 session_start();
-$_SESSION['user']='C@1234';
 if(isset($_SESSION['user']))
     {
-    echo"$_SESSION[user]";
     $con=mysqli_connect('localhost','root','','evento');
-    $query="SELECT e.title,e.description,e.category FROM events as e, applied as a WHERE e.id=a.event_id ";
+    $query="SELECT e.title,e.description,e.category,a.applied_time FROM events as e, applied as a WHERE e.id=a.event_id ";
     $res=mysqli_query($con,$query);
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Event Title</th><th>Event Description</th><th>Category</th>"; 
-    echo "</tr>";
+    echo"<table class='table table-striped'>";
+    echo"<thead><th scope='col'>Title</th><th scope='col'>Description</th><th scope='col'>category</th><th scope='col'>Applied  Date</th></tr></thead><tbody>";
     while($row=mysqli_fetch_assoc($res)){
-        echo "<tr>";
-        echo "<td>$row[title]</td><td>$row[description]</td><td>$row[category]</td>";
-        echo "</tr>";
-
-        }
-        echo "</table>";
-}
+    echo"<tr><th scope='row'>$row[title]</th> <td>$row[description]</td> <td>$row[category]</td> <td>$row[applied_time]</td> </tr>";  
+    }
+    echo"</tbody></table>";
+    }
 ?>
